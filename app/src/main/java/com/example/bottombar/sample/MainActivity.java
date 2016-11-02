680a127a117e24471cc9f5b5893350d396b38bc8
Anish Paul <kinganish688@gmail.com>
2016-11-02 18:39:25 +0530

    1. Updated gradle version from 2.13 to 2.14.1  2. Updated build gradle version from 2.1.2 to 2.2.2  3. Updated android-maven-gradle-plugin:1.3 to 1.4.1  4. Updated MainActivity.class to a much better defined code  5.  Added accent colors to activity_main.xml

--- app/src/main/java/com/example/bottombar/sample/MainActivity.java
+++ app/src/main/java/com/example/bottombar/sample/MainActivity.java
@@ -4,43 +4,68 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     @SuppressWarnings("ConstantConditions")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.simple_three_tabs).setOnClickListener(this);
-        findViewById(R.id.five_tabs_changing_colors).setOnClickListener(this);
-        findViewById(R.id.three_tabs_quick_return).setOnClickListener(this);
-        findViewById(R.id.five_tabs_custom_colors).setOnClickListener(this);
-        findViewById(R.id.badges).setOnClickListener(this);
-    }
+        //Buttons Declaration
 
-    @Override
-    public void onClick(View v) {
-        Class clazz = null;
+        Button simplethreetabs = (Button) findViewById(R.id.simple_three_tabs);
+        Button fivetabscc = (Button) findViewById(R.id.five_tabs_changing_colors);
+        Button threetabsqr = (Button) findViewById(R.id.three_tabs_quick_return);
+        Button fivetabscustomcolors = (Button) findViewById(R.id.five_tabs_custom_colors);
+        Button badges = (Button) findViewById(R.id.badges);
 
-        switch (v.getId()) {
-            case R.id.simple_three_tabs:
-                clazz = ThreeTabsActivity.class;
-                break;
-            case R.id.five_tabs_changing_colors:
-                clazz = FiveColorChangingTabsActivity.class;
-                break;
-            case R.id.three_tabs_quick_return:
-                clazz = ThreeTabsQRActivity.class;
-                break;
-            case R.id.five_tabs_custom_colors:
-                clazz = CustomColorAndFontActivity.class;
-                break;
-            case R.id.badges:
-                clazz = BadgeActivity.class;
-                break;
-        }
+        //Button Declaration ends here
 
-        startActivity(new Intent(this, clazz));
+        //Button Action declaration starts here
+
+        simplethreetabs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, ThreeTabsActivity.class);
+                MainActivity.this.startActivity(i);
+            }
+        });
+
+        fivetabscc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, FiveColorChangingTabsActivity.class);
+                MainActivity.this.startActivity(i);
+            }
+        });
+
+        threetabsqr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, ThreeTabsQRActivity.class);
+                MainActivity.this.startActivity(i);
+            }
+        });
+
+        fivetabscustomcolors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, CustomColorAndFontActivity.class);
+                MainActivity.this.startActivity(i);
+            }
+        });
+
+        badges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, BadgeActivity.class);
+                MainActivity.this.startActivity(i);
+            }
+        });
+
+        // Button action declaration ends here
+
     }
 }
